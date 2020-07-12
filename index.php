075452d8c2ab185ba166e4ff1e8dd62d0ba6e33f
James Waring <jameswaring7@gmail.com>
2020-07-12 13:43:11 +0300

    first commit, basic file structure

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Learn Algorithm Tracing</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css">
+    <script type="text/javascript" link="scripts/main.js">
+</head>
+<body>
+<p>Test this page</p>
+</body>
+</html>
