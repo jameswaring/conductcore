46d532c1675ad480a28b2587e0dc2b7f1726c8de
James Waring <jameswaring7@gmail.com>
2020-07-12 14:19:14 +0300

    added basic login page

--- index.php
+++ index.php
@@ -4,11 +4,18 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Learn Algorithm Tracing</title>
+    <title>Track Behaviour, Effectively Intervene</title>
     <link rel="stylesheet" type="text/css" href="css/styles.css">
-    <script type="text/javascript" link="scripts/main.js">
+    <script type="text/javascript" link="scripts/accountControl.js"></script>
 </head>
 <body>
-<p>Test this page</p>
+<h1>Welcome to Conduct Core</h1>
+<p>Please login with your credentials below</p>
+<form action="login.php" method="post">
+    School: <input type="text" name="schoolInput">
+    Username: <input type="text" name="usernameInput">
+    Password: <input type="text" name="passwordInput">
+    <input type="button" name="loginSubmit" onclick="return validateLogin()">
+</form>
 </body>
 </html>
---
+++ scripts/accountControl.js
@@ -0,0 +1,22 @@
+function validateLogin() {
+    var school = document.getElementsByName("schoolInput")[0].value
+    var username = document.getElementsByName("usernameInput")[0].value
+    var password = document.getElementsByName("passwordInput")[0].value
+    var failed = false;
+    if(!school){
+        document.getElementById("erschool").innerHTML = "No school entered";
+        failed = true;
+    }
+    if(!username){
+        document.getElementById("eruser").innerHTML = "No username entered";
+        failed = true;
+    }
+    if(!password){
+        document.getElementById("erpass").innerHTML = "No password entered";
+        failed = true;
+    }
+    if(failed){
+        return false;
+    }
+    return true;
+}
