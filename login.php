d7c9044654c8551efd8eb4bdf4fe0b711534bba6
James Waring <jameswaring7@gmail.com>
2020-07-19 00:19:10 +0300

    pre local server push

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
 <?php
 
-session_start();
-include 'includes/db_connection.php';
+//session_start();
+//include 'includes/db_connection.php';
 
-$school = htmlspecialchars($_POST['schoolInput']);
-$username = htmlspecialchars($_POST['usernameInput']);
-$password = htmlspecialchars($_POST['passInput']);
-$testVar = connectionTest();
+//$school = htmlspecialchars($_POST['schoolInput']);
+// $username = htmlspecialchars($_POST['usernameInput']);
+// $password = htmlspecialchars($_POST['passInput']);
+// $testVar = connectionTest();
 echo 'here';
-echo $testVar;
-header("Location: home.php");
+// echo $testVar;
+// header("Location: home.php");
 ?>
