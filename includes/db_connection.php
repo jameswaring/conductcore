53f4f6be5262883d8439dfa40c03df4b41f226c8
James Waring <jameswaring7@gmail.com>
2020-07-19 21:45:44 +0300

    switched sql

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,4 +1,14 @@
 <?php
-    $client = new MongoDB\Client('mongodb+srv://james:conductcore123!@primarycluster.hgyaj.mongodb.net/<dbname>?retryWrites=true&w=majority');
-    return($client);
+    function OpenCon(){
+        //pass the database details to variables
+        $host = "localhost";
+        $dbuser = "id14279363_admin";
+        $dbpass = "Noentry123!!!";
+        $dbname = "id14279363_conductore";
+        // combine host and db name in to single variable
+        $dbhost = "mysql:host=$host;dbname=$dbname";
+        //create PDO from database information
+        $dbconn = new PDO($dbhost, $dbuser, $dbpass);
+        return $dbconn;
+}
 ?>
--- login.php
+++ login.php
@@ -1,24 +1,29 @@
 <?php
 
-session_start();
 include 'includes/db_connection.php';
 
 if((!isset($$_POST['schoolInput'])) || (!isset($$_POST['usernameInput'])) || (!isset($$_POST['passInput']))){
-    header("Location: index.php");
+    try {
+    $dbconn = OpenCon();
+    $username = "Harry";
+    $password = "mypass";
+    $school = "slc";
+    $sqlstmnt2 = 'INSERT INTO users(username, password, school) VALUES(:username, :password, :school)';
+    $stmtUsr2 = $dbconn -> prepare($sqlstmnt2);
+    $stmtUsr2 -> bindValue(':username', $username);
+    $stmtUsr2 -> bindValue(':password', $password);
+    $stmtUsr2 -> bindValue(':school', $school);
+    $stmtUsr2 -> execute();
+    echo 'problem with post';
+    } 
+    catch (PDOException $e) {
+        echo "DataBase Error: The user could not be added.<br>".$e->getMessage();
+    } 
+    catch (Exception $e) {
+        echo "General Error: The user could not be added.<br>".$e->getMessage();
+    }
 }
 else{
-    $school = htmlspecialchars($_POST['schoolInput']);
-    $username = htmlspecialchars($_POST['usernameInput']);
-    $password = htmlspecialchars($_POST['passInput']);
-
-    $users = $client->db->users;
-    $document = array( 
-        "username" => "Deny", 
-        "password" => "1234"
-     );
-    $users->insertOne($document);
-
-    header("Location: home.php");
+    echo 'have run get client';
 }
-
 ?>
