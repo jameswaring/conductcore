718d0c4672f825df4b9bbef73a860363824cf48f
James Waring <jameswaring7@gmail.com>
2020-09-27 13:17:29 +0300

    migrated to ClearDB

--- includes/db_connection.php
+++ includes/db_connection.php
@@ -1,10 +1,15 @@
 <?php
     function OpenCon(){
         //pass the database details to variables
-        $host = "db4free.net";
-        $dbuser = "jamesadmin";
-        $dbpass = "Noentry12!";
-        $dbname = "conductcore";
+        $host = "eu-cdbr-west-03.cleardb.net";
+        $dbuser = "b9dcd08c3b5fb6";
+        $dbpass = "114ae453";
+        $dbname = "`heroku_353f881dea3f669`";
+        // old db4free creds
+        //$host = "db4free.net";
+        //$dbuser = "jamesadmin";
+        //$dbpass = "Noentry12!";
+        //$dbname = "conductcore";
         // combine host and db name in to single variable
         $dbhost = "mysql:host=$host;dbname=$dbname";
         //create PDO from database information
