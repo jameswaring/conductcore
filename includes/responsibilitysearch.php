f5ff512d02b24d67377efe94254dc8b703953015
James Waring <jameswaring7@gmail.com>
2020-10-03 16:00:59 +0300

    altered group find code to differentiate between roles

--- includes/responsibilitysearch.php
+++ includes/responsibilitysearch.php
@@ -5,23 +5,52 @@ function staffResponsibilities($id){
     ob_start();
     include_once 'includes/db_connection.php';
         try {
+            // check type of user and return either YEAR GROUP, FORM, ALL or NONE
             $dbconn = OpenCon();
-            $sqlstmnt2 = 'SELECT studentID FROM responsibilities WHERE staffID= :staffID';
-            $stmtUsr2 = $dbconn -> prepare($sqlstmnt2);
+            $sqlstmnt2 = 'SELECT job FROM users WHERE userID= :userID';
             $intid = intval($id);
-            $stmtUsr2 -> bindValue(':staffID', $intid);
+            $stmtUsr2 = $dbconn -> prepare($sqlstmnt2);
+            $stmtUsr2 -> bindValue(':userID', $intid);
             $stmtUsr2 -> execute();
-            $rows = $stmtUsr2->fetchAll(PDO::FETCH_ASSOC);
-            if(sizeof($rows) > 0)
-            {
-                //list found pupils
+            $rows = $stmtUsr2->fetchColumn();
+            var_dump($rows);
+            //now check their responsibility
+            //1 --- head
+            //2 --- headofyear
+            //3 --- form teacher
+            //4 --- non form teacher - return no pupils
+            
+            if($rows == "1"){
+                //return all pupils
+                $dbconn = OpenCon();
+                $sqlstmnt2 = 'SELECT * FROM students';
+                $intid = intval($id);
+                $stmtUsr2 = $dbconn -> prepare($sqlstmnt2);
+                $stmtUsr2 -> bindValue(':userID', $intid);
+                $stmtUsr2 -> execute();
+                $rows = $stmtUsr2->fetchColumn();
                 return $rows;
             }
+            else if($rows == "2"){
+                //return all year or years
+                return $rows;
+            }
+            else if($rows == "3"){
+                //return form
+                $dbconn = OpenCon();
+                $sqlstmnt2 = 'SELECT group FROM responsibilities WHERE userID= :userID';
+                $intid = intval($id);
+                $stmtUsr2 = $dbconn -> prepare($sqlstmnt2);
+                $stmtUsr2 -> bindValue(':userID', $intid);
+                $stmtUsr2 -> execute();
+                $rows = $stmtUsr2->fetchColumn();
+                
+            }
             else{
-                // not responible for any
-                return "none";
-               }
-            } 
+                // return no pupils
+                return 
+            }
+
         catch (PDOException $e) {
             echo "DataBase Error: The user could not be found.<br>".$e->getMessage();
         } 
